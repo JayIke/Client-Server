c810414e94f40e3d69abd710bd6b9fe34b6b0143
Tyler <tylerdburrell@gmail.com>
2020-10-20 21:09:24 -0500

    MongoDB Connection
    Connection to MongoDB

---
+++ demo/src/main/java/com/example/demo/Message.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "messages")
+public class Message {
+    @Id
+    private String id;
+
+    private String author;
+
+    private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+    // getters and setters
+}
